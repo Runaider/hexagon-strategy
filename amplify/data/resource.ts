84ba5b164704141e0b547ffcbc8647b3e37bba08
Rafal J <jarmolkowicz@gmail.com>
2025-01-13 13:13:52 +0200

    app config updated

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,13 +1,49 @@
 import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
+// {
+//   rows: 25,
+//   cols: 25,
+//   hexSize: 50,
+//   maxToxicTilesPerGame: 3,
+//   toxicTileProbability: 0.1,
+//   maxTurns: 20,
+//   previewTileCount: 3,
+//   questProbability: 0.2,
+//   maxQuestsPerGame: 3,
+//   perTurnResourceProduction: false,
+//   actionPrices: {
+//     rotate: {
+//       wood: 0,
+//       stone: 1,
+//       food: 0,
+//       gold: 1,
+//     },
+//     changeUpcomingHex: {
+//       wood: 1,
+//       stone: 0,
+//       food: 1,
+//       gold: 0,
+//     },
+//     redrawUpcomingHexes: {
+//       wood: 1,
+//       stone: 1,
+//       food: 1,
+//       gold: 1,
+//     },
+//   },
+// }
+
 const schema = a.schema({
     AppConfig: a.model({
       rows: a.integer(),
       cols: a.integer(),
       hexSize: a.integer(),
+      maxToxicTilesPerGame: a.integer(),
       toxicTileProbability: a.float(),
       maxTurns: a.integer(),
       previewTileCount: a.integer(),
+      questProbability: a.float(),
+      maxQuestsPerGame: a.integer(),
       perTurnResourceProduction: a.boolean(),
       actionPrices: a.customType({
         rotate: a.customType({

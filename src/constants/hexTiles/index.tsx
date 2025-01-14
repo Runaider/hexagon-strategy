9744282329cdf5e634ae9e4bcb1e9e6b12941ea7
Rafal J <jarmolkowicz@gmail.com>
2025-01-14 10:29:31 +0200

    new tiles

--- src/constants/hexTiles/index.tsx
+++ src/constants/hexTiles/index.tsx
@@ -36,15 +36,6 @@ Tile_TOXIC.sides[3].img = TOXIC_r0;
 Tile_TOXIC.sides[4].img = TOXIC_r0;
 Tile_TOXIC.sides[5].img = TOXIC_r0;
 
-const Tile_FFFCCC = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
 const Tile_FFFFFF = new Tile([
   TileSectionType.Forest,
   TileSectionType.Forest,
@@ -104,14 +95,74 @@ Tile_CCPPPP.sides[3].img = CCPPPP_r3;
 Tile_CCPPPP.sides[4].img = CCPPPP_r4;
 Tile_CCPPPP.sides[5].img = CCPPPP_r5;
 
-const Tile_MMMCCC = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
+const Tile_CCFFFF = new Tile([
   TileSectionType.City,
   TileSectionType.City,
-  TileSectionType.City,
+  TileSectionType.Forest,
+  TileSectionType.Forest,
+  TileSectionType.Forest,
+  TileSectionType.Forest,
 ]);
+
+import CCFFFF_r0 from "../../assets/ccffff/CCFFFF_r0.png";
+import CCFFFF_r1 from "../../assets/ccffff/CCFFFF_r5.png";
+import CCFFFF_r2 from "../../assets/ccffff/CCFFFF_r4.png";
+import CCFFFF_r3 from "../../assets/ccffff/CCFFFF_r3.png";
+import CCFFFF_r4 from "../../assets/ccffff/CCFFFF_r2.png";
+import CCFFFF_r5 from "../../assets/ccffff/CCFFFF_r1.png";
+
+Tile_CCFFFF.sides[0].img = CCFFFF_r0;
+Tile_CCFFFF.sides[1].img = CCFFFF_r1;
+Tile_CCFFFF.sides[2].img = CCFFFF_r2;
+Tile_CCFFFF.sides[3].img = CCFFFF_r3;
+Tile_CCFFFF.sides[4].img = CCFFFF_r4;
+Tile_CCFFFF.sides[5].img = CCFFFF_r5;
+
+const Tile_CCMMMM = new Tile([
+  TileSectionType.City,
+  TileSectionType.City,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+]);
+
+import CCMMMM_r0 from "../../assets/ccmmmm/CCMMMM_r0.png";
+import CCMMMM_r1 from "../../assets/ccmmmm/CCMMMM_r5.png";
+import CCMMMM_r2 from "../../assets/ccmmmm/CCMMMM_r4.png";
+import CCMMMM_r3 from "../../assets/ccmmmm/CCMMMM_r3.png";
+import CCMMMM_r4 from "../../assets/ccmmmm/CCMMMM_r2.png";
+import CCMMMM_r5 from "../../assets/ccmmmm/CCMMMM_r1.png";
+
+Tile_CCMMMM.sides[0].img = CCMMMM_r0;
+Tile_CCMMMM.sides[1].img = CCMMMM_r1;
+Tile_CCMMMM.sides[2].img = CCMMMM_r2;
+Tile_CCMMMM.sides[3].img = CCMMMM_r3;
+Tile_CCMMMM.sides[4].img = CCMMMM_r4;
+Tile_CCMMMM.sides[5].img = CCMMMM_r5;
+
+const Tile_FFMMMM = new Tile([
+  TileSectionType.Forest,
+  TileSectionType.Forest,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+]);
+
+import FFMMMM_r0 from "../../assets/ffmmmm/FFMMMM_r0.png";
+import FFMMMM_r1 from "../../assets/ffmmmm/FFMMMM_r5.png";
+import FFMMMM_r2 from "../../assets/ffmmmm/FFMMMM_r4.png";
+import FFMMMM_r3 from "../../assets/ffmmmm/FFMMMM_r3.png";
+import FFMMMM_r4 from "../../assets/ffmmmm/FFMMMM_r2.png";
+import FFMMMM_r5 from "../../assets/ffmmmm/FFMMMM_r1.png";
+
+Tile_FFMMMM.sides[0].img = FFMMMM_r0;
+Tile_FFMMMM.sides[1].img = FFMMMM_r1;
+Tile_FFMMMM.sides[2].img = FFMMMM_r2;
+Tile_FFMMMM.sides[3].img = FFMMMM_r3;
+Tile_FFMMMM.sides[4].img = FFMMMM_r4;
+Tile_FFMMMM.sides[5].img = FFMMMM_r5;
 
 const Tile_MMMMMM = new Tile([
   TileSectionType.Mountains,
@@ -154,33 +205,6 @@ Tile_MMPPPP.sides[3].img = MMPPPP_r3;
 Tile_MMPPPP.sides[4].img = MMPPPP_r4;
 Tile_MMPPPP.sides[5].img = MMPPPP_r5;
 
-const Tile_WWWCCC = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_WWWWWW = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_PPPCCC = new Tile([
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
 const Tile_PPPPPP = new Tile([
   TileSectionType.Plains,
   TileSectionType.Plains,
@@ -193,78 +217,11 @@ const Tile_PPPPPP = new Tile([
 import PPPPPP_r0 from "../../assets/pppppp/PPPPPP_r0.png";
 
 Tile_PPPPPP.sides[0].img = PPPPPP_r0;
-
-const Tile_CCCMMM = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-]);
-
-const Tile_CCCWWW = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_CCCPPP = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_MMMWWW = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_MMMPPP = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_WWWPPP = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_FFFMMM = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-]);
-
-const Tile_FFFWWW = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
+Tile_PPPPPP.sides[1].img = PPPPPP_r0;
+Tile_PPPPPP.sides[2].img = PPPPPP_r0;
+Tile_PPPPPP.sides[3].img = PPPPPP_r0;
+Tile_PPPPPP.sides[4].img = PPPPPP_r0;
+Tile_PPPPPP.sides[5].img = PPPPPP_r0;
 
 const Tile_FFPPPP = new Tile([
   TileSectionType.Forest,
@@ -289,284 +246,120 @@ Tile_FFPPPP.sides[3].img = FFPPPP_r3;
 Tile_FFPPPP.sides[4].img = FFPPPP_r4;
 Tile_FFPPPP.sides[5].img = FFPPPP_r5;
 
-const Tile_FFFPPP = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_CCCFFF = new Tile([
+const Tile_MMCCCC = new Tile([
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
   TileSectionType.City,
   TileSectionType.City,
   TileSectionType.City,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
+  TileSectionType.City,
 ]);
 
-const Tile_MMMFFF = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
+import MMCCCC_r0 from "../../assets/mmcccc/MMCCCC_r0.png";
+import MMCCCC_r1 from "../../assets/mmcccc/MMCCCC_r5.png";
+import MMCCCC_r2 from "../../assets/mmcccc/MMCCCC_r4.png";
+import MMCCCC_r3 from "../../assets/mmcccc/MMCCCC_r3.png";
+import MMCCCC_r4 from "../../assets/mmcccc/MMCCCC_r2.png";
+import MMCCCC_r5 from "../../assets/mmcccc/MMCCCC_r1.png";
 
-const Tile_WWWFFF = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Water,
+Tile_MMCCCC.sides[0].img = MMCCCC_r0;
+Tile_MMCCCC.sides[1].img = MMCCCC_r1;
+Tile_MMCCCC.sides[2].img = MMCCCC_r2;
+Tile_MMCCCC.sides[3].img = MMCCCC_r3;
+Tile_MMCCCC.sides[4].img = MMCCCC_r4;
+Tile_MMCCCC.sides[5].img = MMCCCC_r5;
+
+const Tile_MMFFFF = new Tile([
+  TileSectionType.Mountains,
+  TileSectionType.Mountains,
+  TileSectionType.Forest,
   TileSectionType.Forest,
   TileSectionType.Forest,
   TileSectionType.Forest,
 ]);
 
-const Tile_PPPFFF = new Tile([
+import MMFFFF_r0 from "../../assets/mmffff/MMFFFF_r0.png";
+import MMFFFF_r1 from "../../assets/mmffff/MMFFFF_r5.png";
+import MMFFFF_r2 from "../../assets/mmffff/MMFFFF_r4.png";
+import MMFFFF_r3 from "../../assets/mmffff/MMFFFF_r3.png";
+import MMFFFF_r4 from "../../assets/mmffff/MMFFFF_r2.png";
+import MMFFFF_r5 from "../../assets/mmffff/MMFFFF_r1.png";
+
+Tile_MMFFFF.sides[0].img = MMFFFF_r0;
+Tile_MMFFFF.sides[1].img = MMFFFF_r1;
+Tile_MMFFFF.sides[2].img = MMFFFF_r2;
+Tile_MMFFFF.sides[3].img = MMFFFF_r3;
+Tile_MMFFFF.sides[4].img = MMFFFF_r4;
+Tile_MMFFFF.sides[5].img = MMFFFF_r5;
+
+const Tile_PPCCCC = new Tile([
   TileSectionType.Plains,
   TileSectionType.Plains,
+  TileSectionType.City,
+  TileSectionType.City,
+  TileSectionType.City,
+  TileSectionType.City,
+]);
+
+import PPCCCC_r0 from "../../assets/ppcccc/PPCCCC_r0.png";
+import PPCCCC_r1 from "../../assets/ppcccc/PPCCCC_r5.png";
+import PPCCCC_r2 from "../../assets/ppcccc/PPCCCC_r4.png";
+import PPCCCC_r3 from "../../assets/ppcccc/PPCCCC_r3.png";
+import PPCCCC_r4 from "../../assets/ppcccc/PPCCCC_r2.png";
+import PPCCCC_r5 from "../../assets/ppcccc/PPCCCC_r1.png";
+
+Tile_PPCCCC.sides[0].img = PPCCCC_r0;
+Tile_PPCCCC.sides[1].img = PPCCCC_r1;
+Tile_PPCCCC.sides[2].img = PPCCCC_r2;
+Tile_PPCCCC.sides[3].img = PPCCCC_r3;
+Tile_PPCCCC.sides[4].img = PPCCCC_r4;
+Tile_PPCCCC.sides[5].img = PPCCCC_r5;
+
+const Tile_PPFFFF = new Tile([
   TileSectionType.Plains,
+  TileSectionType.Plains,
+  TileSectionType.Forest,
   TileSectionType.Forest,
   TileSectionType.Forest,
   TileSectionType.Forest,
 ]);
 
-const Tile_FFMMPP = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
+import PPFFFF_r0 from "../../assets/ppffff/PPFFFF_r0.png";
+import PPFFFF_r1 from "../../assets/ppffff/PPFFFF_r5.png";
+import PPFFFF_r2 from "../../assets/ppffff/PPFFFF_r4.png";
+import PPFFFF_r3 from "../../assets/ppffff/PPFFFF_r3.png";
+import PPFFFF_r4 from "../../assets/ppffff/PPFFFF_r2.png";
+import PPFFFF_r5 from "../../assets/ppffff/PPFFFF_r1.png";
+
+Tile_PPFFFF.sides[0].img = PPFFFF_r0;
+Tile_PPFFFF.sides[1].img = PPFFFF_r1;
+Tile_PPFFFF.sides[2].img = PPFFFF_r2;
+Tile_PPFFFF.sides[3].img = PPFFFF_r3;
+Tile_PPFFFF.sides[4].img = PPFFFF_r4;
+Tile_PPFFFF.sides[5].img = PPFFFF_r5;
+
+const Tile_PPMMMM = new Tile([
   TileSectionType.Plains,
   TileSectionType.Plains,
-]);
-
-const Tile_MMPPWW = new Tile([
   TileSectionType.Mountains,
   TileSectionType.Mountains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_PPWWCC = new Tile([
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_WWCCFF = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_CCMMPP = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_MMPPCC = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_PPCCWW = new Tile([
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_CCWWFF = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_WWFFMM = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
   TileSectionType.Mountains,
   TileSectionType.Mountains,
 ]);
 
-const Tile_FFMMCC = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
+import PPMMMM_r0 from "../../assets/ppmmmm/PPMMMM_r0.png";
+import PPMMMM_r1 from "../../assets/ppmmmm/PPMMMM_r5.png";
+import PPMMMM_r2 from "../../assets/ppmmmm/PPMMMM_r4.png";
+import PPMMMM_r3 from "../../assets/ppmmmm/PPMMMM_r3.png";
+import PPMMMM_r4 from "../../assets/ppmmmm/PPMMMM_r2.png";
+import PPMMMM_r5 from "../../assets/ppmmmm/PPMMMM_r1.png";
 
-const Tile_MMCCWW = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_CCWWPP = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_WWPPFF = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_FFCCMM = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-]);
-
-const Tile_MMCCPP = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
-
-const Tile_CCPPWW = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_PPWWFF = new Tile([
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_WWFFCC = new Tile([
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_CCMMFF = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_MMFFWW = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_FFWWCC = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_CCFFMM = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-]);
-
-const Tile_MMFFCC = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.City,
-  TileSectionType.City,
-]);
-
-const Tile_CCMMWW = new Tile([
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-]);
-
-const Tile_MMWWFF = new Tile([
-  TileSectionType.Mountains,
-  TileSectionType.Mountains,
-  TileSectionType.Water,
-  TileSectionType.Water,
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-]);
-
-const Tile_FFCCPP = new Tile([
-  TileSectionType.Forest,
-  TileSectionType.Forest,
-  TileSectionType.City,
-  TileSectionType.City,
-  TileSectionType.Plains,
-  TileSectionType.Plains,
-]);
+Tile_PPMMMM.sides[0].img = PPMMMM_r0;
+Tile_PPMMMM.sides[1].img = PPMMMM_r1;
+Tile_PPMMMM.sides[2].img = PPMMMM_r2;
+Tile_PPMMMM.sides[3].img = PPMMMM_r3;
+Tile_PPMMMM.sides[4].img = PPMMMM_r4;
+Tile_PPMMMM.sides[5].img = PPMMMM_r5;
 
 const allTiles = [
   //full
@@ -577,59 +370,24 @@ const allTiles = [
   // half tiles
 
   // quarter tiles
+  Tile_PPCCCC,
+  Tile_PPFFFF,
+  Tile_PPMMMM,
+
   Tile_FFPPPP,
+  Tile_FFMMMM,
+
   Tile_CCPPPP,
+  Tile_CCFFFF,
+  Tile_CCMMMM,
+
   Tile_MMPPPP,
+  Tile_MMCCCC,
+  Tile_MMFFFF,
+
   // =====
-  // Tile_FFFCCC,
-  // Tile_FFFFFF,
-  // Tile_CCCCCC,
-  // Tile_MMMCCC,
-  // Tile_MMMMMM,
-  // Tile_WWWCCC,
-  // Tile_WWWWWW,
-  // Tile_PPPCCC,
-  // Tile_PPPPPP,
-  // Tile_CCCMMM,
-  // Tile_CCCWWW,
-  // Tile_CCCPPP,
-  // Tile_MMMWWW,
-  // Tile_MMMPPP,
-  // Tile_WWWPPP,
-  // Tile_FFFMMM,
-  // Tile_FFFWWW,
-  // Tile_FFFPPP,
-  // Tile_CCCFFF,
-  // Tile_MMMFFF,
-  // Tile_WWWFFF,
-  // Tile_PPPFFF,
+
   // //   3part tiles
-  // Tile_FFMMPP,
-  // Tile_MMPPWW,
-  // Tile_PPWWCC,
-  // Tile_WWCCFF,
-  // Tile_CCMMPP,
-  // Tile_MMPPCC,
-  // Tile_PPCCWW,
-  // Tile_CCWWFF,
-  // Tile_WWFFMM,
-  // Tile_FFMMCC,
-  // Tile_MMCCWW,
-  // Tile_CCWWPP,
-  // Tile_WWPPFF,
-  // Tile_FFCCMM,
-  // Tile_MMCCPP,
-  // Tile_CCPPWW,
-  // Tile_PPWWFF,
-  // Tile_WWFFCC,
-  // Tile_CCMMFF,
-  // Tile_MMFFWW,
-  // Tile_FFWWCC,
-  // Tile_CCFFMM,
-  // Tile_MMFFCC,
-  // Tile_CCMMWW,
-  // Tile_MMWWFF,
-  // Tile_FFCCPP,
 ];
 
 export {
@@ -637,30 +395,17 @@ export {
   Tile_CASTLE,
   Tile_TOXIC,
   Tile_FFPPPP,
+  Tile_FFMMMM,
   Tile_FFFFFF,
   Tile_CCCCCC,
   Tile_CCPPPP,
+  Tile_CCFFFF,
+  Tile_CCMMMM,
   Tile_PPPPPP,
-
-  // Tile_FFFCCC,
-  // Tile_CCCCCC,
-  // Tile_MMMCCC,
-  // Tile_MMMMMM,
-  // Tile_WWWCCC,
-  // Tile_WWWWWW,
-  // Tile_PPPCCC,
-  // Tile_PPPPPP,
-  // Tile_CCCMMM,
-  // Tile_CCCWWW,
-  // Tile_CCCPPP,
-  // Tile_MMMWWW,
-  // Tile_MMMPPP,
-  // Tile_WWWPPP,
-  // Tile_FFFMMM,
-  // Tile_FFFWWW,
-  // Tile_FFFPPP,
-  // Tile_CCCFFF,
-  // Tile_MMMFFF,
-  // Tile_WWWFFF,
-  // Tile_PPPFFF,
+  Tile_PPCCCC,
+  Tile_PPFFFF,
+  Tile_PPMMMM,
+  Tile_MMPPPP,
+  Tile_MMCCCC,
+  Tile_MMFFFF,
 };
